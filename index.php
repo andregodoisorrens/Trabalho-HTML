c5acc229ebcabcad0c5d2277edaa85cec0fabd1d
andregodoisorrens <andregodoisorrens961@gmail.com>
2021-03-14 18:37:37 -0300

    adicionei arqivos e melhorei o index.html

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
  <?php
 
-echo "Olá " . $_POST["nome"] . " (email: " . $_POST["email"] . ")<br><br>";
+echo "Olá, é um te prazer conhecer pedimos para que confirme suas informações " . $_POST["email"] . ")<br><br>";
 echo "Sua mensagem: " . $_POST["mensagem"];
-
+echox $_POST["apelido"]
 ?>
 
