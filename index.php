65a44aeaa9c0c3f81ba7622b9a28fb771a4a25c0
andregodoisorrens <andregodoisorrens961@gmail.com>
2021-03-14 18:03:36 -0300

    adicione o arquivo index.html

--- index.php
+++ index.php
@@ -1,21 +1,7 @@
-<!DOCTYPE html>
-<html lang="pt-br">
-  <head>
-    <meta charset="UTF-8" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Site Teste</title>
-  </head>
-  <body>
-  <form action="index.php" method="post">
-Nome: <input type=text name=nome><br>
-Email: <input type=text name=email><br><br>
-Mensagem: <textarea name=mensagem cols=8 rows=3></textarea><br>
-<input type=submit>
-</form>
-<?php
+ <?php
+
 echo "Olá " . $_POST["nome"] . " (email: " . $_POST["email"] . ")<br><br>";
 echo "Sua mensagem: " . $_POST["mensagem"];
+
 ?>
-  </body>
-</html>
+
