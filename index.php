b43ccf7d6d423950f34d29ef26c239122c4d260b
andregodoisorrens <andregodoisorrens961@gmail.com>
2021-03-14 17:52:58 -0300

    adicionei informações ao index.php

--- index.php
+++ index.php
@@ -7,16 +7,15 @@
     <title>Site Teste</title>
   </head>
   <body>
-    <h1>tentativa 1</h1>
-    <h3>Tentativa de colocar um site no ar</h3>
-    <?php
-    <input type="text" value="$N">
-    if ($N>10){
-      echo "Você Pode acessar";
-    }
-    else{
-      echo "Você Não pode passar";
-    }
-    
+  <form action="index.php" method="post">
+Nome: <input type=text name=nome><br>
+Email: <input type=text name=email><br><br>
+Mensagem: <textarea name=mensagem cols=8 rows=3></textarea><br>
+<input type=submit>
+</form>
+<?php
+echo "Olá " . $_POST["nome"] . " (email: " . $_POST["email"] . ")<br><br>";
+echo "Sua mensagem: " . $_POST["mensagem"];
+?>
   </body>
 </html>
